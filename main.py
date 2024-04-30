a26a79042e1c88fbc6fa74a5f7220c19d9b3b766
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2024-04-30 11:11:48 +0200

    submit assessment

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ Multiplication Table
 Palindromes
 """
 # Provide your solution here
+def my_fun():
+    print("test")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
